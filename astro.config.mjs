d0ff9d1b73cc53fa29ba902172e6833e0352f765
Sebastian W <Sebastian.wittekindt@gmail.com>
2026-04-23 10:05:11 -0400

    Fixed base URL oops

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,17 +27,17 @@ export default defineConfig({
                     "Automatically generate maps for Jet Lag The Game: Hide and Seek with ease! Simply name the questions and watch the map eliminate hundreds of possibilities in seconds.",
                 icons: [
                     {
-                        src: "https://sebzdead.github.io/JetLagHideAndSeek/JLIcon.png",
+                        src: "https://sebzdead.github.io/MontrealHideAndSeek/JLIcon.png",
                         sizes: "1080x1080",
                         type: "image/png",
                     },
                     {
-                        src: "https://sebzdead.github.io/JetLagHideAndSeek/android-chrome-192x192.png",
+                        src: "https://sebzdead.github.io/MontrealHideAndSeek/android-chrome-192x192.png",
                         sizes: "192x192",
                         type: "image/png",
                     },
                     {
-                        src: "https://sebzdead.github.io/JetLagHideAndSeek/android-chrome-512x512.png",
+                        src: "https://sebzdead.github.io/MontrealHideAndSeek/android-chrome-512x512.png",
                         sizes: "512x512",
                         type: "image/png",
                     },
@@ -92,5 +92,5 @@ export default defineConfig({
         enabled: false,
     },
     site: "https://sebzdead.github.io",
-    base: "JetLagHideAndSeek",
+    base: "MontrealHideAndSeek",
 });
